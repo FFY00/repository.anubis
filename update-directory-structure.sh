cd338bd1fd59b8278da53ec2fc3466764cdd3994
Filipe Laíns <ffy00.gnc@gmail.com>
2017-11-19 18:23:31 +0000

    Update directory generation script

--- update-directory-structure.sh
+++ update-directory-structure.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Generate md5 checksum for every .zip file
+ for ZIP in $(find . -name '*.zip'); do
+   (
+   md5sum ${ZIP} | grep -o '^\S*' > ${ZIP}.md5
+   )
+done
+
+# Update addons.xml md5 checksum
+md5sum addons.xml | grep -o '^\S*' > addons.xml.md5
+
+# Generate index.html files
 for DIR in $(find . -type d); do
   (
   echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
@@ -6,4 +18,3 @@ for DIR in $(find . -type d); do
   echo -e "</pre>\n</body>\n</html>"
   ) > "${DIR}/index.html"
 done
-
