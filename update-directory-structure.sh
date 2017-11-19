69dbb663fcc6eb5c0b92de85b75f3a6b6b4593a9
Filipe Laíns <ffy00.gnc@gmail.com>
2017-11-19 19:11:09 +0000

    Improved the update-directory-structure.sh script

--- update-directory-structure.sh
+++ update-directory-structure.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
+# Check if it's in the right directory
+ROOT=$(pwd)
+if [[ "$ROOT" != *repository.anubis ]]
+then
+  echo -e "Run this script inside the repository root directory!"
+  exit 1
+fi
+echo "Root Path: ${ROOT}"
+
 # Generate md5 checksum for every .zip file
- for ZIP in $(find . -name '*.zip'); do
-   (
+for ZIP in $(find ${ROOT} -name '*.zip'); do
    md5sum ${ZIP} | grep -o '^\S*' > ${ZIP}.md5
-   )
+   echo "Updated ${ZIP}.md5"
 done
 
 # Update addons.xml md5 checksum
-md5sum addons.xml | grep -o '^\S*' > addons.xml.md5
+md5sum ${ROOT}/addons.xml | grep -o '^\S*' > ${ROOT}/addons.xml.md5
+echo "Updated ${ROOT}/addons.xml.md5"
 
 # Generate index.html files
-for DIR in $(find . -type d); do
+for DIR in $(find ${ROOT} -path ${ROOT}/.git -prune -o -type d); do
   (
   echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
   ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$"  | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
   echo -e "</pre>\n</body>\n</html>"
   ) > "${DIR}/index.html"
+  echo "Updated ${DIR}/index.html"
 done
