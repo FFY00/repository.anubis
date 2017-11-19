f2fcad6ace352471173dd3e671405ec610753835
Filipe Laíns <ffy00.gnc@gmail.com>
2017-11-19 19:19:51 +0000

    Remove listing of .git directory

--- update-directory-structure.sh
+++ update-directory-structure.sh
@@ -23,7 +23,7 @@ echo "Updated ${ROOT}/addons.xml.md5"
 for DIR in $(find ${ROOT} -path ${ROOT}/.git -prune -o -type d); do
   (
   echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
-  ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$"  | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
+  ls -1pa -I.git "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$"  | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
   echo -e "</pre>\n</body>\n</html>"
   ) > "${DIR}/index.html"
   echo "Updated ${DIR}/index.html"
